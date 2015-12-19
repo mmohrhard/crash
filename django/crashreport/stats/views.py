8d6e7867c7f0c2b1d3326d4bd72702d76ba346e8
Markus Mohrhard <markus.mohrhard@googlemail.com>
2015-12-19 23:15:39 +0100

    add a crashes by version page

--- django/crashreport/stats/views.py
+++ django/crashreport/stats/views.py
@@ -7,6 +7,7 @@
 
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.db.models import Count
 
 from processor.models import ProcessedCrash, Signature
 from django.contrib.staticfiles import finders
@@ -26,11 +27,31 @@ def signature(request, signature):
     return render(request, 'stats/signature.html', {'signature':signature_obj, 'crashes':crashes})
 
 
-
 def top_crashes(request):
     return HttpResponse('top crashes')
 
+class CrashByVersionData(object):
+    def __init__(self, id):
+        self.all = 0
+        self.win = 0
+        self.lin = 0
+        self.id = id
+
 def crashes_by_version(request, version):
-    return HttpResponse('crashes for version: %s' % (version))
+    res = ProcessedCrash.objects.filter(crash_id__version__major_version=5, crash_id__version__minor_version=1).values('os_name', 'signature').annotate(Count('os_name'))
+    data = {}
+    for entry in res:
+        signature = entry['signature']
+        if not signature in data:
+            data[signature] = CrashByVersionData(signature)
+        count = entry['os_name__count']
+        data[signature].all += count
+        if entry['os_name'] == 'linux':
+            data[signature].lin = count
+        elif entry['os_name'] == 'windows':
+            data[signature].win = count
+
+    print(data.values())
+    return render(request, 'stats/version.html', {'signatures':data.values()})
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab: */
