3f03224dfad29d813127ddf809bd8dbd86c4d519
Markus Mohrhard <markus.mohrhard@googlemail.com>
2015-12-22 17:27:26 +0100

    show crash counts in the admin interface

--- django/crashreport/processor/admin.py
+++ django/crashreport/processor/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import ProcessedCrash, Signature
+from .models import ProcessedCrash, Signature, CrashCount
 
 admin.site.register(ProcessedCrash)
 admin.site.register(Signature)
+admin.site.register(CrashCount)
