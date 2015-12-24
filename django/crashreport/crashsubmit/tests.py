325ea7779692f831fb3e314a796c37e6f42e7a67
Markus Mohrhard <markus.mohrhard@googlemail.com>
2015-12-24 16:35:39 +0100

    add test for Product string

--- django/crashreport/crashsubmit/tests.py
+++ django/crashreport/crashsubmit/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Product
+
+class ProductModelTest(TestCase):
+    def test_product_string(self):
+        product = Product(product_name="TestProduct")
+        product_str = str(product)
+        self.assertEqual(product_str, "TestProduct")
