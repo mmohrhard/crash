a9a873b3beebb5bf608ccb1bcf1c1fd6c1d79472
Markus Mohrhard <markus.mohrhard@googlemail.com>
2015-12-24 16:51:04 +0100

    add initial test for Version model

--- django/crashreport/crashsubmit/tests.py
+++ django/crashreport/crashsubmit/tests.py
@@ -1,9 +1,20 @@
 from django.test import TestCase
 
-from .models import Product
+from .models import Product, Version
 
 class ProductModelTest(TestCase):
     def test_product_string(self):
         product = Product(product_name="TestProduct")
         product_str = str(product)
         self.assertEqual(product_str, "TestProduct")
+
+class VersionModeltest(TestCase):
+    def setUp(self):
+        self.product = Product(product_name="TestProduct")
+        self.product.save()
+
+    def test_version_string(self):
+        version = Version(product=self.product, major_version=1,
+                minor_version=2, micro_version=3,
+                patch_version=4)
+        self.assertEqual(str(version), "TestProduct Version: 1.2.3.4")
