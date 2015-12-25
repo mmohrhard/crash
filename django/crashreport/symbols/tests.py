2b17d9b1b1846c69e06d31664edc8ac3d0b5cbc8
Markus Mohrhard <markus.mohrhard@googlemail.com>
2015-12-25 22:27:45 +0100

    add initial test for symbols upload view  # Changes not staged for commit:

--- django/crashreport/symbols/tests.py
+++ django/crashreport/symbols/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+import os, zipfile
+
+def get_test_file_path(file_name):
+    dir = os.path.dirname(__file__)
+    return os.path.join(dir, "testdata/%s" % (file_name))
+
+class TestSimpleSymbolsUpload(TestCase):
+
+    def test_symbols_upload_valid_zip(self):
+        c = Client()
+        with open(get_test_file_path("valid.zip")) as f:
+            response = c.post('/upload/', {'symbols':f, 'comment': 'Test Comment'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_sybols_upload_invalid_zip(self):
+        c = Client()
+        with open(get_test_file_path("invalid.zip")) as f:
+            with self.assertRaises(zipfile.BadZipfile):
+                response = c.post('/upload/', {'symbols': f, 'comment': 'Test Comment'})
