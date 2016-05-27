9906303d7509a6e735dfb1e09244e2c9e9ca6fe0
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-05-27 18:38:01 +0200

    add first test for processing of uploaded crashes

--- django/crashreport/processor/tests.py
+++ django/crashreport/processor/tests.py
@@ -1,14 +1,30 @@
 from django.test import TestCase
 
-from .models import CrashCount
+from .models import CrashCount, ProcessedCrash
+from .processor import MinidumpProcessor
 
 from base.models import Version
+from crashsubmit.models import UploadedCrash
 
 import datetime
+import os
+import tempfile
+
 from django.utils import timezone
 
 def get_date_n_days_ago(num_days):
     return datetime.date.fromordinal(datetime.date.today().toordinal() - num_days)
+
+def get_test_file_path(file_name):
+    dir = os.path.dirname(__file__)
+    return os.path.join(dir, file_name)
+
+def remove_dir(top):
+    for root, dirs, files in os.walk(top, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
 
 class CrashCountManagerTest(TestCase):
 
@@ -49,3 +65,27 @@ class CrashCountManagerTest(TestCase):
 
         res = CrashCount.objects.get_crash_count(time=5, versions=[self.version2])
         self.assertEqual(len(res), 0)
+
+class ProcessCrashTest(TestCase):
+
+    def setUp(self):
+        self.version1 = Version.objects.create(
+                major_version=1, minor_version=2, micro_version=3, patch_version=4)
+
+        self.submitted_crash = UploadedCrash.objects.create(
+                crash_id = 'some id', version = self.version1,
+                crash_path = get_test_file_path("testdata/test.dmp"),
+                additional_data = '{ "key1": "value1" }')
+
+        self.tmp_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        remove_dir(self.tmp_dir)
+
+    def test_process_crash(self):
+        processor = MinidumpProcessor()
+
+        processor.process('some id')
+
+        processed_crash = ProcessedCrash.objects.get(crash_id = 'some id')
+        self.assertIsNotNone(processed_crash)
