0c40c25d3425510ed65916a65a42bd5d0336d26b
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-06-08 14:54:15 +0200

    add missing symbols test

--- django/crashreport/symbols/tests.py
+++ django/crashreport/symbols/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
 from django.test import Client
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 import os, zipfile, tempfile
 
 from .models import SymbolsUpload
+
+from processor.models import ProcessedCrash, Signature
 
 def get_test_file_path(file_name):
     dir = os.path.dirname(__file__)
@@ -56,3 +59,35 @@ class TestSimpleSymbolsUpload(TestCase):
         with self.settings(SYMBOL_UPLOAD_DIR=self.tmp_dir):
             response = self.c.post('/upload/', {'comment': 'test comment'})
         self.assertEqual(response.status_code, 405)
+
+class TestMissingSymbols(TestCase):
+
+    def setUp(self):
+        self.signature = Signature()
+        self.signature.signature = 'test'
+        self.signature.first_observed = timezone.now()
+        self.signature.last_observed = timezone.now()
+
+        self.crash = ProcessedCrash()
+        self.crash.modules = "Module|soffice.bin|5.3.0.0|soffice.bin|4B253D6CB7E740A997444A61FE6E511C2|0x010d0000|0x01178fff|1\nModule|mergedlo.dll|5.3.0.0|mergedlo.pdb|6C797FEC36EF447699D43D58FE1486102|0x66290000|0x6a401fff|0\nModule|actxprxy.dll|6.1.7601.17514|ActXPrxy.pdb|C674D3ABFBB34B75BC59063E6B68ABA12|0x6a710000|0x6a75dfff|0"
+        self.crash.upload_time = timezone.now()
+        self.crash.signature = self.signature
+        self.crash.save()
+
+        self.tmp_dir = tempfile.mkdtemp()
+
+        user = User.objects.create_user('test user')
+        self.c = Client()
+        self.c.force_login(user)
+
+    def tearDwon(self):
+        remove_dir(self.tmp_dir)
+
+    def test_get_missing_symbols(self):
+        with self.settings(SYMBOL_DIR=self.tmp_dir):
+            response = self.c.get('/upload/missing')
+        content = response.content;
+        content_split = content.splitlines()
+        self.assertIn("ActXPrxy.pdb,C674D3ABFBB34B75BC59063E6B68ABA12", content_split)
+        self.assertIn("soffice.bin,4B253D6CB7E740A997444A61FE6E511C2", content_split)
+        self.assertIn("mergedlo.pdb,6C797FEC36EF447699D43D58FE1486102", content_split)
