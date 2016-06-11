a25bbca9b2e6af6d29adce1f4ccdb2573e77a43e
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-06-12 00:57:40 +0200

    fix typo

--- django/crashreport/processor/processor.py
+++ django/crashreport/processor/processor.py
@@ -26,7 +26,7 @@ class MinidumpProcessor(object):
     def process(self, crash_id):
 
         if len(ProcessedCrash.objects.filter(crash_id=crash_id)) != 0:
-            logger.error("trying to process teh same uploaded crash twice")
+            logger.error("trying to process the same uploaded crash twice")
             raise IntegrityError('object already in db')
 
         original_crash_report = submit_model.UploadedCrash.objects.get(crash_id=crash_id)
