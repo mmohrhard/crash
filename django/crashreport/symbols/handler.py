009a6c3c434b91be8aa4fa53c0de68ac465992c3
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-11-21 16:12:56 +0100

    remove missing symbol entries for uploaded symbols
    This fixes #28.

--- django/crashreport/symbols/handler.py
+++ django/crashreport/symbols/handler.py
@@ -7,10 +7,12 @@
 
 from zipfile import ZipFile
 
-from .models import SymbolsUpload
+from .models import SymbolsUpload, MissingSymbol
 
 from django.utils import timezone
 from django.conf import settings
+
+import os
 
 class SymbolsUploadHandler(object):
 
@@ -19,7 +21,8 @@ class SymbolsUploadHandler(object):
 
     def process(self, data, path):
         zip_file = ZipFile(path)
-        file_names = "\n".join(zip_file.namelist())
+        filename_list = zip_file.namelist()
+        file_names = "\n".join(filename_list)
         zip_file.extractall(settings.SYMBOL_LOCATION)
 
         upload = SymbolsUpload()
@@ -27,6 +30,19 @@ class SymbolsUploadHandler(object):
         upload.comment = data['version'] + " " + data['platform']
         upload.upload_time = timezone.now()
         upload.system_symbols = data['system'] if 'system' in data else False;
+
+        if upload.system_symbols is True:
+            self._handle_missing_system_symbols(filename_list)
         upload.save()
+
+    def _handle_missing_system_symbols(self, filenames):
+        debug_ids = []
+        for filename in filenames:
+            try:
+                debug_ids.append(os.path.split(os.path.split(filename)[0])[1])
+            except:
+                pass
+
+        MissingSymbol.objects.filter(debug_id__in = debug_ids).delete()
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab: */
