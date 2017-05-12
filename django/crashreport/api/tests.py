d8af481054adffda01e7a6160d3e1bcc247ffe3e
Markus Mohrhard <markus.mohrhard@googlemail.com>
2017-05-12 14:10:09 +0200

    implement better crash count generation

--- django/crashreport/api/tests.py
+++ django/crashreport/api/tests.py
@@ -2,8 +2,10 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase, Client
+from django.utils import timezone
 
 from base.models import Version
+from processor.models import ProcessedCrash, Signature
 
 # Create your tests here.
 
@@ -12,8 +14,16 @@ class ApiCrashCountTest(TestCase):
     def setUp(self):
         self.version1 = Version.objects.create(
                 major_version=1, minor_version=2, micro_version=3, patch_version=4)
+        self.signature = Signature.objects.create(signature='cde', first_observed= timezone.now(), last_observed = timezone.now())
 
     def test_post(self):
         c = Client()
-        response = c.post('/api/get/crash-count/')
-        self.assertEqual(response.status_code, 404)
+        response = c.post('/api/get/crash-count')
+        self.assertEqual(response.status_code, 405)
+
+    def test_simple(self):
+        ProcessedCrash.objects.create(crash_id='abc', version = self.version1,
+                os_name = 'Linux', signature = self.signature, upload_time = timezone.now())
+        c = Client()
+        response = c.get('/api/get/crash-count')
+        print(response.content)
--- django/crashreport/api/views.py
+++ django/crashreport/api/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponseNotAllowed
 from django.views.decorators.csrf import csrf_exempt
+from django.db.models import Count
 
 from processor.models import ProcessedCrash, Signature, CrashCount, BugReport
 from base.models import Version
@@ -13,11 +14,12 @@ def api_crash_count(request):
     if request.method != 'GET':
         return HttpResponseNotAllowed('Only GET allowed')
 
-    data = {'versions': {}}
-    for v in Version.objects.all():
-        version = v.str_without_product()
-        crashes = ProcessedCrash.objects.get_crashes_for_version(version=version).count()
-        data['versions'][version] = crashes
+    version_info = ProcessedCrash.objects.all().values('version').annotate(Count('version'))
+    data = {}
+    for version in version_info:
+        data[Version.objects.get(id = version['version']).str_without_product()] = version['version__count']
+
+    data = {'versions': data}
     return JsonResponse(data)
 
 # Create your views here.
