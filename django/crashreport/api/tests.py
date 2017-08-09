cfe48206209328a30dcb6157af74642e9bfdeb71
AndyMtz04 <andres.mtz044@gmail.com>
2017-08-09 14:52:52 -0500

    add testing for chart data API

--- django/crashreport/api/tests.py
+++ django/crashreport/api/tests.py
@@ -27,3 +27,39 @@ class ApiCrashCountTest(TestCase):
         c = Client()
         response = c.get('/api/get/crash-count')
         print(response.content)
+
+class ApiChartDataTest(TestCase):
+
+    valid_days = [1, 3, 7, 14, 28]
+    invalid_days = [4, 8, 16, 25, 55]
+
+    def test_valid_days_get(self):
+        """
+        Tests server response with a GET request and valid days
+        """
+        c = Client()
+
+        for i in self.valid_days:
+            response = c.get('/api/get/chart-data/{}'.format(i))
+            self.assertEqual(response.status_code, 200)
+
+    def test_invalid_days_get(self):
+        """
+        Tests server response with a GET request and invalid days
+        """
+        c = Client()
+
+        for i in self.invalid_days:
+            response = c.get('/api/get/chart-data/{}'.format(i))
+            self.assertEqual(response.status_code, 400)
+
+    def test_post(self):
+        """
+        Tests server response with a POST request and valid days
+        """
+        c = Client()
+
+        for i in self.valid_days:
+            response = c.post('/api/get/chart-data/{}'.format(i))
+            self.assertEqual(response.status_code, 405)
+            print(response.content)
